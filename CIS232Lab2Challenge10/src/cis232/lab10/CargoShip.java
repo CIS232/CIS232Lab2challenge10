4805f512bbf27914a44010ed2c24417ac5377620
SK207_24 <SK207_24@K207_24.carrollcc.edu>
2016-09-19 20:54:39 -0400

    CargoShip class

---
+++ CIS232Lab2Challenge10/src/cis232/lab10/CargoShip.java
@@ -0,0 +1,10 @@
+package cis232.lab10;
+
+public class CargoShip extends Ship {
+
+	public CargoShip(String shipName, String yearBuilt) {
+		super(shipName, yearBuilt);
+		// TODO Auto-generated constructor stub
+	}
+
+}
